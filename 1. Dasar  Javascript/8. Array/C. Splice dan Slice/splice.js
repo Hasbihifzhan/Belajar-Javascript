50d9982e163a6f609e9ae3d668efacbe59840deb
Hasbihifzhan <hifzhanmuhammadhasbi5@gmail.com>
2024-10-25 09:44:51 +0700

    Belajar

--- 1. Dasar  Javascript/8. Array/C. Splice dan Slice/splice.js
+++ 1. Dasar  Javascript/8. Array/C. Splice dan Slice/splice.js
@@ -10,12 +10,12 @@
     // slice menghasilkan array baru
     // slice(awal,akhir);
     var arr1 = ["Hasbi","Hifzhan","Muhammad","Helmi"];
-    var arr2 = arr.slice(1,3);
-    console.log(arr2.join(' - '));
+    // var arr2 = arr.slice(1,3);
+    // console.log(arr2.join(' - '));
     // jika kita ingin memunculkan index ke 1 & 2 maka penulisannya arr.slice(1,3) bukan (1,2)
     // index awalnya terbawa index akhirnya dikurangi 1
 
     // splice : menyambung/menambal bisa menyispkan elemen ditengah-tegah arraynya
     // splice(indexAwal, maudihapusberapa, elemenBaru1, elemenBaru2, .....)
-    // arr.splice(2, 0, 'fizhbieries');
-    // console.log(arr.join(" - "));
+    arr.splice(2, 0, 'fizhbieries');
+    console.log(arr.join(" - "));
